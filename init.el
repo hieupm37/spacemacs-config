a70a491c3e1d7830267caf161a2fa26c35dfe62d
Hieu Phan <hieupm@coccoc.com>
2019-04-16 11:00:29 +0700

    Add javascript, php, python layer.

--- init.el
+++ init.el
@@ -52,6 +52,9 @@ values."
      ;; spell-checking
      ;; syntax-checking
      ;; version-control
+     javascript
+     php
+     python
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
